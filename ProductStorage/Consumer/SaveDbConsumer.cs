9a31e4a5a7ed45788c3f1e103542ef38ae52d33d
BenyaminMusavi <beni97d@gmail.com>
2023-02-04 23:59:30 +0330

    Add project files.

---
+++ ProductStorage/Consumer/SaveDbConsumer.cs
@@ -0,0 +1,33 @@
+﻿using Contracts;
+using MassTransit;
+
+namespace ProductStorage.Consumer
+{
+    public class SaveDbConsumer : IConsumer<Product>
+    {
+        private readonly List<Product> _products = new List<Product>();
+        public async Task Consume(ConsumeContext<Product> context)
+        {
+            await Task.Delay(5000).ContinueWith(task =>
+               _products.Add(new Product
+               {
+                   Id = context.Message.Id,
+                   Amount = context.Message.Amount,
+                   DateTime = context.Message.DateTime,
+                   ProductName = context.Message.ProductName,
+               })
+            );
+
+            Console.WriteLine($"insert db {context.Message.Id}");
+
+            await context.RespondAsync(new ResponseMessage { MessageName = "Successed" });
+
+            //if (_products.Count == 50)
+            //{
+            //    var message = _products;
+
+            //}
+            //   return Task.CompletedTask;
+        }
+    }
+}
---
+++ ProductStorage/Program.cs
@@ -0,0 +1,55 @@
+using Contracts;
+using MassTransit;
+using ProductStorage.Consumer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+Console.WriteLine("Product Storage");
+
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer(typeof(SaveDbConsumer));
+    x.SetKebabCaseEndpointNameFormatter();
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        //cfg.Host(builder.Configuration.GetValue<string>("RabbitConnection"));
+        cfg.Host(new Uri("rabbitmq://localhost/"), hst =>
+        {
+            hst.Username(Constants.UserName);
+            hst.Password(Constants.Password);
+        });
+
+        cfg.ConfigureEndpoints(context);
+        cfg.PrefetchCount = 16;
+        //cfg.ReceiveEndpoint(Constants.SendServiceQueue, e =>
+        //{
+        //    e.Consumer<SaveDbConsumer>();
+        //    e.PrefetchCount = 16;
+        //});
+    });
+    x.AddRequestClient<Product>();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
